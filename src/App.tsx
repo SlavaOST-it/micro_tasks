0fa3292b5c3db734b542b810aa24866e592240d8
Vyacheslav Ostapkevich <slavaost-it@mail.ru>
2022-06-24 19:15:31 +0300

    Add Task02

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-import Task01 from "./tasks/task_01/Task_01";
+import Task01 from "./tasks/task_01/Task01";
+import Task02 from "./tasks/task_02/Task02";
 
 
 const App = () => {
     return (
         <div className="App">
             <Task01/>
+            <Task02/>
         </div>
     );
 }
---
+++ src/tasks/task_02/Task02.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import './App.css';
+import Button from './Button'
+
+
+const Task02 = () => {
+    const Button1Foo = (subscriber:string, age:number) => {
+        console.log(subscriber, age)
+    }
+
+    const Button2Foo = (subscriber:string) => {
+        console.log(subscriber)
+    }
+
+    const Button3Foo=()=>{
+        console.log("Im stupid button")
+    }
+
+    return (
+        <div className="App">
+            <Button name={"YouTubeChenel-1"} callBack={()=>Button1Foo("Im Vasya", 21)}/>
+            <Button name={"YouTubeChenel-2"} callBack={()=>Button2Foo("Im Ivan")}/>
+            <Button name={"Im stupid button"} callBack={Button3Foo}/>
+        </div>
+    );
+}
+
+export default Task02;
